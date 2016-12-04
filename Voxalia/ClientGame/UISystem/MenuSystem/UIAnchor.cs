8c06cd5fd4bb47dc934dba473638d64504920ec9
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-12-03 18:33:36 -0800

    License header correction

--- Voxalia/ClientGame/UISystem/MenuSystem/UIAnchor.cs
+++ Voxalia/ClientGame/UISystem/MenuSystem/UIAnchor.cs
@@ -1,4 +1,4 @@
-﻿//
+//
 // This file is part of the game Voxalia, created by FreneticXYZ.
 // This code is Copyright (C) 2016 FreneticXYZ under the terms of the MIT license.
 // See README.md or LICENSE.txt for contents of the MIT license.
