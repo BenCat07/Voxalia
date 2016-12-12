f3315d86869e69e394de15b9a1cbde01bcea4303
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-12-11 23:15:25 -0800

    add an additional potential model loading warning

--- Voxalia/ClientGame/GraphicsSystems/ModelEngine.cs
+++ Voxalia/ClientGame/GraphicsSystems/ModelEngine.cs
@@ -489,6 +489,7 @@ namespace Voxalia.ClientGame.GraphicsSystems
             if (Engine.TheClient.Files.Exists("models/" + Name + ".skin"))
             {
                 string[] data = Engine.TheClient.Files.ReadText("models/" + Name + ".skin").SplitFast('\n');
+                int c = 0;
                 foreach (string datum in data)
                 {
                     if (datum.Length > 0)
@@ -525,6 +526,7 @@ namespace Voxalia.ClientGame.GraphicsSystems
                                     {
                                         SysConsole.Output(OutputType.WARNING, "Unknown skin entry typer: '" + typer + "', expected reflectivity, specular, or simply no specification!");
                                     }
+                                    c++;
                                     success = true;
                                 }
                             }
@@ -540,6 +542,10 @@ namespace Voxalia.ClientGame.GraphicsSystems
                             }
                         }
                     }
+                }
+                if (c == 0)
+                {
+                    SysConsole.Output(OutputType.WARNING, "No entries in " + Name + ".skin");
                 }
             }
             else
