9aa7c4315f2f0e87f24d2cf697f85e918c73d48a
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-12-23 05:02:34 -0800

    Update all license headers to something more generic for the moment

--- Voxalia/openvr_api.cs
+++ Voxalia/openvr_api.cs
@@ -1,13 +1,20 @@
+//
+// This file is part of the game Voxalia, created by FreneticXYZ.
+// This code is Copyright (C) 2016 FreneticXYZ under the terms of a strict license.
+// See README.md or LICENSE.txt in the source root for contents of the license.
+// If neither of these are not available, assume that neither you nor anyone other than the copyright holder
+// hold any right or permission to use this software until such time as the official license is identified.
+//
+
+using System;
+using System.Runtime.InteropServices;
+
 //======= Copyright (c) Valve Corporation, All rights reserved. ===============
 //
 // Purpose: This file contains C#/managed code bindings for the OpenVR interfaces
 // This file is auto-generated, do not edit it.
 //
 //=============================================================================
-
-using System;
-using System.Runtime.InteropServices;
-using Valve.VR;
 
 namespace Valve.VR
 {
