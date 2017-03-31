17d01bf49159383130c81cedcd2478fec2acadd7
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2017-03-31 01:20:54 -0700

    Some minor irrelevant cleaning

--- Voxalia/Shared/Collision/AABB.cs
+++ Voxalia/Shared/Collision/AABB.cs
@@ -11,12 +11,26 @@ using FreneticGameCore;
 
 namespace Voxalia.Shared.Collision
 {
+    /// <summary>
+    /// Represents an Axis-Aligned Bounding Box.
+    /// </summary>
     public class AABB
     {
+        /// <summary>
+        /// The minimum coordinates.
+        /// </summary>
         public Location Min;
 
+        /// <summary>
+        /// The maximum coordinates.
+        /// </summary>
         public Location Max;
 
+        /// <summary>
+        /// Returns whether the box intersects another box.
+        /// </summary>
+        /// <param name="box2">The second box.</param>
+        /// <returns>Whether they intersect.</returns>
         public bool Intersects(AABB box2)
         {
             Location min2 = box2.Min;
@@ -24,6 +38,10 @@ namespace Voxalia.Shared.Collision
             return !(min2.X > Max.X || max2.X < Min.X || min2.Y > Max.Y || max2.Y < Min.Y || min2.Z > Max.Z || max2.Z < Min.Z);
         }
 
+        /// <summary>
+        /// Includes a Location into the box's space, expanding as needed (but not shrinking).
+        /// </summary>
+        /// <param name="pos">The position to include.</param>
         public void Include(Location pos)
         {
             if (pos.X < Min.X)
