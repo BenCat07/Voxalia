3b700cf2e076bd8502762aefc921a24f816bdeae
mcmonkey4eva <agoodwin@freneticllc.com>
2017-06-06 21:47:03 -0700

    Doc entity types

--- Voxalia/Shared/EntityType.cs
+++ Voxalia/Shared/EntityType.cs
@@ -13,26 +13,86 @@ using System.Text;
 
 namespace Voxalia.Shared
 {
+    /// <summary>
+    /// All know entity types in the base game. This is NOT a definitive list (EG mods, plugins, or version differences may apply).
+    /// </summary>
     public enum EntityType: int
     {
+        /// <summary>
+        /// A standard actual player entity.
+        /// </summary>
         PLAYER = 1,
+        /// <summary>
+        /// An arrow (shot by a bow generally).
+        /// </summary>
         ARROW = 2,
+        /// <summary>
+        /// A group of blocks in the physical world.
+        /// </summary>
         BLOCK_GROUP = 3,
+        /// <summary>
+        /// A singular block in the physical world.
+        /// </summary>
         BLOCK_ITEM = 4,
+        /// <summary>
+        /// A bullet (shot by a gun generally).
+        /// </summary>
         BULLET = 5,
+        /// <summary>
+        /// A glow-stick that lights the area in entity form.
+        /// </summary>
         GLOWSTICK = 6,
+        /// <summary>
+        /// A grenade that gives off smoke for a duration.
+        /// </summary>
         SMOKE_GRENADE = 7,
+        /// <summary>
+        /// A dynamic free model entity in the physical world.
+        /// </summary>
         MODEL = 8,
+        /// <summary>
+        /// An item in the world that may be picked up.
+        /// </summary>
         ITEM = 9,
+        /// <summary>
+        /// A standard drivable car.
+        /// </summary>
         CAR = 10,
+        /// <summary>
+        /// Part of a vehicle (EG a wheel).
+        /// </summary>
         VEHICLE_PART = 11,
+        /// <summary>
+        /// A test-dummy target.
+        /// </summary>
         TARGET_ENTITY = 12,
+        /// <summary>
+        /// A slime monster.
+        /// </summary>
         SLIME = 13,
+        /// <summary>
+        /// A grenade that explodes after a delay.
+        /// </summary>
         EXPLOSIVE_GRENADE = 14,
+        /// <summary>
+        /// A grenade that covers its area with paint after explosion.
+        /// </summary>
         PAINT_BOMB = 15,
+        /// <summary>
+        /// A block that plays music.
+        /// </summary>
         MUSIC_BLOCK = 16,
+        /// <summary>
+        /// A helicopter vehicle.
+        /// </summary>
         HELICOPTER = 17,
+        /// <summary>
+        /// An air-plane vehicle.
+        /// </summary>
         PLANE = 18,
+        /// <summary>
+        /// A hovering text message popup.
+        /// </summary>
         HOVER_MESSAGE = 19
     }
 }
